9c37b2c89be3525f1b57be8ec7a7f8538728ec48
thiago-ulloa-develcode <84195990+thiagoulloa@users.noreply.github.com>
2023-03-10 14:31:43 -0300

    loginpage

--- project-leia/src/pages/loginPage/index.js
+++ project-leia/src/pages/loginPage/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./style.css";
+import LogoLeia from "../images/logoleia.png";
 
 function LoginPage() {
   let navigate = useNavigate();
 
   return (
-    <div className="Home">
+    <div className="home">
       <h1>Login Page</h1>
-      <button onClick={() => navigate("/home-page")}>Home Page</button>
+      <div className="container">
+        <div className="logoDiv">
+          <img id="logo" src={LogoLeia}></img>
+        </div>
+      </div>
     </div>
   );
 }
