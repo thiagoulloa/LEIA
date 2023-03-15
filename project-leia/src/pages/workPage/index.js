2abdf48b43e964e538807e4b42b3de59e4443b28
thiago-ulloa-develcode <84195990+thiagoulloa@users.noreply.github.com>
2023-03-15 11:39:28 -0300

    Ajustando containers

--- project-leia/src/pages/workPage/index.js
+++ project-leia/src/pages/workPage/index.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 
-function Chat() {
+function WorkPage() {
+  let navigate = useNavigate();
+
   const [mensagem, setMensagem] = useState("");
   const [mensagensEnviadas, setMensagensEnviadas] = useState([]);
 
@@ -21,35 +23,24 @@ function Chat() {
   }
 
   return (
-    <div>
-      <div className="container-mensagens">
-        {mensagensEnviadas.map((mensagem, index) => (
-          <div key={index} className="mensagem">
-            {mensagem}
-          </div>
-        ))}
+    <div className="workPage">
+      <div className="containers-div">
+        <div className="container-text code">
+          {mensagensEnviadas.map((mensagem, index) => (
+            <div key={index} className="mensagem">
+              {mensagem}
+            </div>
+          ))}
+        </div>
+        <div className="container-text doc"></div>
       </div>
+
       <form onSubmit={handleSubmit}>
         <input type="text" value={mensagem} onChange={handleChange} />
         <button type="submit" className="glow">
           Enviar
         </button>
       </form>
-    </div>
-  );
-}
-
-function WorkPage() {
-  let navigate = useNavigate();
-
-  return (
-    <div className="workPage">
-      <div className="containerWork">
-        <div className="logoDivWork">
-          <Chat />
-        </div>
-        <div className="align-center"></div>
-      </div>
     </div>
   );
 }
