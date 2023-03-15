27b374f67b54ff6147461085dcbb9d631cc23abf
EmeteZ <mtmacedo17@gmail.com>
2023-03-15 01:04:52 -0300

    Mensagens (BETA)
    provavelmente vai dar ruim isso ai

--- project-leia/src/pages/workPage/index.js
+++ project-leia/src/pages/workPage/index.js
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
-import LogoLeia from "../images/logoleia.png";
+
+function Chat() {
+  const [mensagem, setMensagem] = useState("");
+  const [mensagensEnviadas, setMensagensEnviadas] = useState([]);
+
+  function handleChange(event) {
+    setMensagem(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    enviarMensagem(mensagem);
+    setMensagem("");
+  }
+
+  function enviarMensagem(mensagem) {
+    setMensagensEnviadas([...mensagensEnviadas, mensagem]);
+  }
+
+  return (
+    <div>
+      <div className="container-mensagens">
+        {mensagensEnviadas.map((mensagem, index) => (
+          <div key={index} className="mensagem">
+            {mensagem}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={mensagem} onChange={handleChange} />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
 
 function WorkPage() {
   let navigate = useNavigate();
 
   return (
     <div className="workPage">
-      <div className="containerWork"></div>
-      <div className="align-center"></div>
+      <div className="containerWork">
+        <div className="logoDivWork">
+          <Chat />
+        </div>
+        <div className="align-center"></div>
+      </div>
     </div>
   );
 }
