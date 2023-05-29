75b568c7bf068e8037a964f16b66853d441c906d
EmeteZ <125473215+EmeteZ@users.noreply.github.com>
2023-05-29 15:25:22 -0300

    Introduction Page (Matheus)
    Introduction 90% feita

--- project-leia/src/pages/introductionPage/index.js
+++ project-leia/src/pages/introductionPage/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import LogoLeia from "../../images/logoleia.png";
+import Instagram from "./images/Instagram.png";
 import "./style.css";
 import WebFont from 'webfontloader';
 
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-  MDBIcon,
-  MDBBtn
-} from 'mdb-react-ui-kit';
 
 function IntroductionPage() {
   let navigate = useNavigate();
@@ -20,7 +13,7 @@ function IntroductionPage() {
   
     WebFont.load({
       google: {
-        families: ['Noto Sans:400']
+        families: ['Space Grotesk:500']
       }
     });
    
@@ -53,8 +46,8 @@ function IntroductionPage() {
         <div className="QuadradoBranco">
 
 
-          <div style={{fontFamily: 'Noto Sans'}}>
-<p className ="IntroductionTexto"> Seja bem vindo ao Projeto Léia? <br></br>
+          <div style={{fontFamily: 'Space Grotesk'}}>
+<p className ="IntroductionTexto"> Seja bem vindo ao Projeto Léia! <br></br>
 Aqui você encontrará uma solução rápida e prática para sua documentação de códigos, com nossa inteligência artificial eleve a qualidade de seus projetos e economize seu tempo! </p>
 
           </div>
@@ -64,90 +57,22 @@ Aqui você encontrará uma solução rápida e prática para sua documentação 
     </div>
 
 <div className="footer">
-    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#f1f1f1' }}>
-      <MDBContainer className='pt-4'>
-        <section className='mb-4'>
-          <MDBBtn
-            rippleColor="dark"
-            color='link'
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fab fa-facebook-f' />
-          </MDBBtn>
+    
+<div className="cardF">
 
-          <MDBBtn
-            rippleColor="dark"
-            color='link'
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-twitter' />
-          </MDBBtn>
+<div className="alignLeft"></div>
 
-          <MDBBtn
-            rippleColor="dark"
-            color='link'
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-google' />
-          </MDBBtn>
+  <div className="alignCenter">
+<div style={{fontFamily: 'Space Grotesk'}}>
+  <p className="TextoFT">© 2023 Todos os direitos reservados, Projeto Léia®</p>
+  </div>
+  </div>
+  <div className="alignRight">
+  <img className="logo instagram" src={Instagram}></img>
+  </div>
+</div>
 
-          <MDBBtn
-            rippleColor="dark"
-            color='link'
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-instagram' />
-          </MDBBtn>
 
-          <MDBBtn
-            rippleColor="dark"
-            color='link'
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-linkedin' />
-          </MDBBtn>
-
-          <MDBBtn
-            rippleColor="dark"
-            color='link'
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-github' />
-          </MDBBtn>
-        </section>
-      </MDBContainer>
-
-      <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2020 Copyright:
-        <a className='text-dark' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
-        </a>
-      </div>
-    </MDBFooter>
     </div>
 
 
