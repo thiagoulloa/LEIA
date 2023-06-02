53ee54c2294f5aefdffc598579bca860fdadfd4e
GustavoGaleazzi <74423759+gusata@users.noreply.github.com>
2023-06-02 16:26:01 -0300

    filtro de busca pronto

--- project-leia/src/pages/homePage/index.js
+++ project-leia/src/pages/homePage/index.js
@@ -6,8 +6,9 @@ import { useNavigate } from "react-router-dom";
 import "./style.css";
 import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faSearch } from "@fortawesome/free-solid-svg-icons";
 import Axios from "axios";
+
 
 export default function HomePage() {
   let navigate = useNavigate();
@@ -27,9 +28,26 @@ export default function HomePage() {
     getProjects();
   }, []);
 
-  /* React.useEffect(() => {
+
+
+  function searchProjects() {
+    if(searchValue.length >= 1){
+    Axios.post("http://localhost:3001/search", {
+      id_usuario: state,
+      titulo: searchValue,
+    })
+      .then((response) => {
+        setProjects(response.data)
+        console.log(response)
+      })
+      .catch((error) => console.log(error));
+  }
+  else{
     getProjects();
-  }, [searchValue]); */
+  }
+}
+  
+
 
   function getProjects() {
     Axios.post("http://localhost:3001/getprojects", {
@@ -78,11 +96,16 @@ export default function HomePage() {
         <div className="align-top homepage">
           <h1 id="title-home">Seus Arquivos:</h1>
           <div className="align-right homepage">
+            <div className="search-div">
             <input
               className="search-docs"
+              id="search-input"
               value={searchValue}
-              onChange={(e) => setSearchValue(e.target.value)}
+              onChange={(e) => setSearchValue(e.target.value)
+              }
             ></input>
+            <FontAwesomeIcon className="ft" icon={faSearch} onClick={searchProjects}/>
+            </div>
             <button
               id="new-doc-button"
               onClick={() => navigate("/work-page", { state: state })}
