5f3f63366665e11691ac146f17a3140d47ebdfe1
thiago-ulloa-develcode <84195990+thiagoulloa@users.noreply.github.com>
2023-06-19 18:14:11 -0300

    GPT Implementado (em testes)

--- project-leia/src/pages/workPage/index.js
+++ project-leia/src/pages/workPage/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useRef } from "react";
 import Axios from "axios";
-import JoditEditor from "jodit-react";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import "./style.css";
@@ -12,11 +11,19 @@ function WorkPage() {
   const [mensagem, setMensagem] = useState("");
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [answer, setAnswer] = useState("");
 
   const { state } = useLocation();
 
+  const client = Axios.create({
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_CHATGPT_KEY}`,
+    },
+  });
+
   React.useEffect(() => {
     console.log(state);
+    getProject();
   }, []);
 
   async function SaveDoc() {
@@ -28,6 +35,33 @@ function WorkPage() {
     }).then((response) => {
       console.log(response);
     });
+  }
+
+  function getProject() {
+    Axios.post("http://localhost:3001/getprojectbyid", {
+      projectId: state[0].project,
+    })
+      .then((response) => {
+        setContent(response.data[0].content);
+        setTitle(response.data[0].titulo);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  function sendRequest() {
+    const prompt = `Documente este código separando e explicando suas funções: \n\n"${mensagem}`;
+
+    const params = {
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 1000,
+      temperature: 0.7,
+    };
+
+    client
+      .post("https://api.openai.com/v1/completions", params)
+      .then((result) => setContent(result.data.choices[0].text))
+      .catch((err) => console.log(err));
   }
 
   return (
@@ -44,9 +78,9 @@ function WorkPage() {
             type="text"
             className="input-code"
             value={mensagem}
-            onChange={setMensagem}
+            onChange={(e) => setMensagem(e.target.value)}
           />
-          <button type="submit" className="glow">
+          <button className="glow" onClick={sendRequest}>
             Enviar
           </button>
         </div>
