53baee45416fd83bb94fd31c6469786f399e18fc
EmeteZ <125473215+EmeteZ@users.noreply.github.com>
2023-08-04 16:39:47 -0300

    Melhorando cards de projeto

--- project-leia/src/pages/homePage/components/Card/index.js
+++ project-leia/src/pages/homePage/components/Card/index.js
@@ -2,6 +2,8 @@ import icondc from "../../images/icondc.png";
 import React from "react";
 import "./style.css";
 import { useNavigate } from "react-router-dom";
+import { faUser, faPencilSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function ProjectCard({ titulo, preview, projectId, userId }) {
   let navigate = useNavigate();
@@ -14,17 +16,27 @@ export default function ProjectCard({ titulo, preview, projectId, userId }) {
   ]);
 
   return (
-    <div
-      className="cardHome"
-      onClick={() => navigate("/project-page", { state: info })}
-    >
-      <div className="AlignLeft">
+    <div className="cardHome">
+      <div
+        className="proj-card-left"
+        onClick={() => navigate("/project-page", { state: info })}
+      >
         <div className="Icon">
           <img src={icondc} className="Icon"></img>
         </div>
         <div className="Texto">
           <div className="Titulo">{titulo}</div>
           <div className="Desc">{preview}</div>
+        </div>
+      </div>
+      <div className="AlignRight">
+        <div className="editar">
+          <FontAwesomeIcon
+            className="ico"
+            id="edit-icon"
+            icon={faPencilSquare}
+            onClick={() => navigate("/new-project", { state: info })}
+          />
         </div>
       </div>
     </div>
--- project-leia/src/pages/workPage/index.js
+++ project-leia/src/pages/workPage/index.js
@@ -51,7 +51,7 @@ function WorkPage() {
   }, []);
 
   async function SaveDoc() {
-    Axios.post("http://projetoleia.ddns.net:3001/savedocs", {
+    Axios.post("projetoleia.ddns.net:3001/savedocs", {
       titulo: title,
       content: content,
       preview: content,
@@ -62,7 +62,7 @@ function WorkPage() {
   }
 
   function getDocument() {
-    Axios.post("http://projetoleia.ddns.net:3001/getdocumentbyid", {
+    Axios.post("projetoleia.ddns.net:3001/getdocumentbyid", {
       documentId: state[0].documentId,
     })
       .then((response) => {
